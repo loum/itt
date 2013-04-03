6b69adf18f20c12e662b61c6e43bcc11d3d5d6f0
Philip Jay <philip.jay@softelsystems.com.au>
2013-04-03 16:11:53 +1100

    Added HttpServer and HttpRequestHandler to itt namespace Added a testsuite for HttpServer

--- src/itt/__init__.py
+++ src/itt/__init__.py
@@ -3,3 +3,5 @@ from itt.server import Server
 from itt.client import Client
 from itt.server.tftpserver import TftpServer
 from itt.server.ftpserver import FtpServer
+from itt.server.httpserver import HttpServer
+from itt.server.httprequesthandler import HttpRequestHandler
