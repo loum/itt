1b4f7f6e9b6bf37aaae9b76f81ae00e8c8bb7c02
Philip Jay <philip.jay@softelsystems.com.au>
2013-04-03 16:09:45 +1100

    Split httprequesthandler from httpserver

---
+++ src/itt/server/httprequesthandler.py
@@ -0,0 +1,70 @@
+"""
+@author: pjay
+"""
+
+import hashlib
+import BaseHTTPServer
+
+import itt
+from itt.utils.log import log, class_logging
+
+class HttpRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    """Provides a custom HTTP request handler for ITT purposes.
+    """
+    
+    contentSent = ''
+    
+    def storeAndWrite(self, str, wfile):
+        """Store the string in self.contentSent, and write it to wfile)"""
+        self.contentSent = self.contentSent + str
+        wfile.write(str)
+    
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        
+    def do_GET(self):
+        """Respond to a GET request."""
+        log.info("HTTP GET request received from %s on port %s for %s" % (
+            self.client_address[0],
+            self.client_address[1],
+            self.path,
+        ))
+        
+        path_array = self.path.split('/')
+        
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        
+        self.storeAndWrite(("Selected path %s\n" % path_array[1]), self.wfile)
+        
+        if path_array[1] == 'fast':
+            self.storeAndWrite(("Fastest possible transfer *\n"), self.wfile)
+            self.storeAndWrite(("* as limited by the TCP stack & network\n"), self.wfile)
+            self.wfile.flush()
+            for i in range(1000):
+                self.storeAndWrite(("Line #%s / 1000\n" % i), self.wfile)
+            self.wfile.flush()
+
+        elif path_array[1] == 'slow':
+            self.storeAndWrite(("Slow transfer *\n"), self.wfile)
+            self.storeAndWrite(("* a few bytes every 1 second\n"), self.wfile)
+            self.wfile.flush()
+            for i in range(10):
+                self.storeAndWrite(("Line #%s / 1000\n" % i), self.wfile)
+                time.sleep(1)
+                self.wfile.flush()
+                
+        else:
+            self.storeAndWrite(("You chose %s, which is not valid\n" % path_array[1]), self.wfile)
+            self.storeAndWrite(("Try fast or slow\n"), self.wfile)
+            self.wfile.flush()
+            
+        log.info("HTTP GET request finished for %s on port %s for %s\n   Resulting SHA1 sum of content: %s" % (
+           self.client_address[0],
+           self.client_address[1],
+           self.path,
+           hashlib.sha1(self.contentSent).hexdigest(),
+        ))
--- src/itt/server/httpserver.py
+++ src/itt/server/httpserver.py
@@ -1,85 +1,27 @@
 """
-Created on Mar 6, 2013
-
 @author: pjay
 """
 
-import time
-import hashlib
+##  Hack for running HttpServer from itself; ie. python httpserver.py
+import sys
+sys.path.insert(0, "../..")
+
+##  Normal imports
 import BaseHTTPServer
+from signal import SIGTERM
 
-class IttHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    """
-    classdocs
-    """
-    
-    contentSent = ''
-    
-    def storeAndWrite(self, str, wfile):
-        """Store the string in self.contentSent, and write it to wfile)"""
-        self.contentSent = self.contentSent + str
-        wfile.write(str)
-    
-    def do_HEAD(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        
-    def do_GET(self):
-        """Respond to a GET request."""
-        print time.asctime(), "XXX: GET request received from %s on port %s for %s" % (
-           self.client_address[0],
-           self.client_address[1],
-           self.path,
-        )
-        
-        path_array = self.path.split('/')
-        
-        self.send_response(200)
-        self.send_header("Content-type", "text/plain")
-        self.end_headers()
-        
-        self.storeAndWrite(("Selected path %s\n" % path_array[1]), self.wfile)
-        
-        if path_array[1] == 'fast':
-            self.storeAndWrite(("Fastest possible transfer *\n"), self.wfile)
-            self.storeAndWrite(("* as limited by the TCP stack & network\n"), self.wfile)
-            self.wfile.flush()
-            for i in range(1000):
-                self.storeAndWrite(("Line #%s / 1000\n" % i), self.wfile)
-            self.wfile.flush()
+import itt
+from itt.utils.log import log, class_logging
 
-        elif path_array[1] == 'slow':
-            self.storeAndWrite(("Slow transfer *\n"), self.wfile)
-            self.storeAndWrite(("* a few bytes every 1 second\n"), self.wfile)
-            self.wfile.flush()
-            for i in range(10):
-                self.storeAndWrite(("Line #%s / 1000\n" % i), self.wfile)
-                time.sleep(1)
-                self.wfile.flush()
-                
-        else:
-            self.storeAndWrite(("You chose %s, which is not valid\n" % path_array[1]), self.wfile)
-            self.storeAndWrite(("Try fast or slow\n"), self.wfile)
-            self.wfile.flush()
-            
-        print time.asctime(), "XXX: GET request finished for %s on port %s for %s" % (
-           self.client_address[0],
-           self.client_address[1],
-           self.path,
-        )
-        shasum = hashlib.sha1(self.contentSent).hexdigest()
-        print time.asctime(), "XXX: SHA1 sum of content send to %s on port %s is: %s" % (
-           self.client_address[0],
-           self.client_address[1],
-           shasum,
-        )
 
 class HttpServer(itt.Server):
+    """Provides a HTTP server.
+    """
 
     def __init__(self,
+                 bind='localhost',
                  port=8000,
-                 bind=''):
+                 request_handler=None):
         """HttpServer initialiser.
         """
         super(HttpServer, self).__init__()
@@ -87,7 +29,15 @@ class HttpServer(itt.Server):
         self.port = port
         self.bind = bind
         self.server = None
-        self.handler = None
+        self.handler = request_handler
+
+    def sigterm_handler(self, signum, frame):
+        """Handle being killed.
+        """
+        log_msg = 'HTTP server process'
+        log.info('%s - terminate signalled ...' % log_msg)
+
+        self.stop()
 
     def start(self):
         """Wrapper around the server start process.
@@ -111,9 +61,15 @@ class HttpServer(itt.Server):
         self.server.server_close()
 
 if __name__ == '__main__':
-    myServer = itt.HttpServer()
+    myServer = itt.HttpServer(
+        bind='',
+        request_handler=itt.HttpRequestHandler,
+    )
     try:
+        print "HTTP server starting"
+        print "CTRL+C to terminate"
         myServer.start()
     except KeyboardInterrupt:
         pass
+    print "\nHTTP server terminating"
     myServer.stop()
