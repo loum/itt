e9585607b0fb636973a4cc640ff99791b79138a3
Philip Jay <philip.jay@softelsystems.com.au>
2013-04-03 17:09:09 +1100

    Restructure in preparation for upload function. Work in progress

--- src/itt/client/httpclient.py
+++ src/itt/client/httpclient.py
@@ -11,6 +11,7 @@ sys.path.insert(0, "../..")
 import optparse
 import requests
 import hashlib
+import sys
 
 import itt
 from itt.utils.log import log, class_logging
@@ -18,6 +19,23 @@ from itt.utils.log import log, class_logging
 class HttpClient(itt.Client):
     '''Provides a HTTP client for uploads & downloads
     '''
+
+    def download(self):
+        log.info("HTTP client begins download (HTTP GET): %s" % (self.opts.url))
+        try:
+            download = requests.get(self.opts.url)
+            log.info("HTTP client finishes download (HTTP GET): %s" % (self.opts.url))
+            if download.status_code == 200:
+                shasum = hashlib.sha1(download.content).hexdigest()
+                log.info("SHA1 sum of content received: %s" % (shasum))
+            else:
+                log.error("Download (HTTP GET) failed with status code: %s" % (download.status_code))
+        except requests.ConnectionError, e:
+            log.error("Download (HTTP GET) failed with a connection error: %s" % (str(e)))
+
+    def upload(self):
+        pass
+
 
     def run(self):
 
@@ -31,23 +49,31 @@ class HttpClient(itt.Client):
         parser.add_option('-u', '--url',
             default='http://localhost:8000/',
             help='URL to access [default: %default]')
-        
+
+        parser.add_option('-U', '--upload', action="store_true",
+            default=False,
+            help='Run an upload test (mutually exclusive to --download)')
+
+        parser.add_option('-D', '--download', action="store_true",
+            default=False,
+            help='Run an download test (mutually exclusive to --upload)')
+
         (self.opts, args) = parser.parse_args()
 
         if len(args) != 0:
             log.warning("HTTP client takes no arguments, ignoring the ones provided")
-        
-        log.info("HTTP client begins download (HTTP GET): %s" % (self.opts.url))
-        try:
-            download = requests.get(self.opts.url)
-            log.info("HTTP client finishes download (HTTP GET): %s" % (self.opts.url))
-            if download.status_code == 200:
-                shasum = hashlib.sha1(download.content).hexdigest()
-                log.info("SHA1 sum of content received: %s" % (shasum))
-            else:
-                log.error("Download (HTTP GET) failed with status code: %s" % (download.status_code))
-        except requests.ConnectionError, e:
-            log.error("Download (HTTP GET) failed with a connection error: %s" % (str(e)))
+        print self.opts
+        if self.opts.upload and self.opts.download:
+            log.error("HTTP client can only upload _or_ download, not both")
+        elif not self.opts.upload and not self.opts.download:
+            log.error("HTTP client needs to be told to do an upload or a download")
+        elif self.opts.download:
+            self.download()
+        elif self.opts.upload:
+            self.upload()
+        else:
+            log.error("Something has gone horribly wrong")
+
 
 if __name__ == '__main__':
     myClient = itt.HttpClient()
