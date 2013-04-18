da01be0d367c349f2d53303cb8c419c6b002d72e
Philip Jay <philip.jay@softelsystems.com.au>
2013-04-18 16:30:18 +1000

    Added itt.client.ittclient module:  - generic way to execute a client (protocol independent, etc.)  - can execute clients from the cli, rather than just testsuite
    Modified itt module to export TestConfig and IttClient into the name
    space

--- src/itt/__init__.py
+++ src/itt/__init__.py
@@ -1,5 +1,6 @@
 from itt.server import Server
 from itt.client import Client
+from itt.client.ittclient import IttClient
 from itt.client.httpclient import HttpClient
 from itt.client.tftpclient import TftpClient
 from itt.client.ftpclient import FtpClient
@@ -8,3 +9,4 @@ from itt.server.ftpserver import FtpServer
 from itt.server.httpserver import HttpServer
 from itt.server.httprequesthandler import HttpRequestHandler
 from itt.server.config import ServerConfig
+from itt.test.config import TestConfig
