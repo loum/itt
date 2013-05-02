7ca11a3d5066c78dd2222a959561a94df0291270
Lou Markovski <lou.markovski@softelsystems.com.au>
2013-05-02 20:21:43 +1000

    In general: * UPDATE feature/us27 task/ta30 -- Create test_config model
    control/Makefile
    * Refactored the "test" target using .PHONY tag
    
    control/settings.py
    * add the "test_config" app into the INSTALLED_APPS config setting
    
    control/test_config/models.py
    * updated cut of the test_config model capturing the fields "upload",
      "bytes" and "minimum_gap"
    
    control/test_config/tests/__init__.py
    * added the TestTestConfigModels into the test namespace
    
    control/test_config/tests/__init__.py
    * change to the minimum value check of the TestConfig.bytes attribute to
      accept the value 0.  This will represent "network limited"

--- src/itt/control/settings.py
+++ src/itt/control/settings.py
@@ -129,6 +129,7 @@ INSTALLED_APPS = (
     'test',
     'config',
     'server',
+    'test_config',
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
--- src/itt/control/test_config/models.py
+++ src/itt/control/test_config/models.py
@@ -1,3 +1,79 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class IntegerRangeField(models.IntegerField):
+
+    def __init__(self,
+                 verbose_name=None,
+                 name=None,
+                 min_value=None,
+                 max_value=None,
+                 **kwargs):
+        self.min_value = min_value
+        self.max_value = max_value
+        models.IntegerField.__init__(self,
+                                     verbose_name,
+                                     name,
+                                     **kwargs)
+
+    def formfield(self, **kwargs):
+        defaults = {'min_value': self.min_value,
+                    'max_value': self.max_value}
+        defaults.update(kwargs)
+
+        return super(IntegerRangeField, self).formfield(**defaults)
+
+
+class FloatRangeField(models.FloatField):
+
+    def __init__(self,
+                 verbose_name=None,
+                 name=None,
+                 min_value=None,
+                 max_value=None,
+                 **kwargs):
+        self.min_value = min_value
+        self.max_value = max_value
+        models.FloatField.__init__(self,
+                                   verbose_name,
+                                   name,
+                                   **kwargs)
+
+    def formfield(self, **kwargs):
+        defaults = {'min_value': self.min_value,
+                    'max_value': self.max_value}
+        defaults.update(kwargs)
+
+        return super(FloatRangeField, self).formfield(**defaults)
+
+
+class TestConfig(models.Model):
+
+    upload = models.BooleanField()
+    bytes = IntegerRangeField(default=0,
+                              min_value=0,
+                              validators=[MinValueValidator(0)])
+    minimum_gap = FloatRangeField(min_value=0,
+                                  validators=[MinValueValidator(0)])
+
+    def get_fields(self):
+        """Sugar function that returns a list of tuples representing
+        the key, value pair of the TestConfig model object.
+
+        Example usage::
+
+        >>> from test_config.models import TestConfig
+        >>> test_config_list = TestConfig.objects.all()
+        >>> for test_config in test_config_list:
+        ...     test_config.get_fields()
+        ...
+        [('id', u'1'), ('upload', u'True') ...]
+
+        **Returns:**
+            list of tuples representing the (key, value) pair of the
+            TestConfig model object.
+
+        """
+        return [(f.name,
+                 f.value_to_string(self)) for f in TestConfig._meta.fields]
