e4bb5c76f207cdce1a3fa97f8d3bcf1522bb5e27
Philip Jay <philip.jay@softelsystems.com.au>
2013-05-08 16:48:10 +1000

    Add itt.test and also add whitespace

--- src/itt/__init__.py
+++ src/itt/__init__.py
@@ -1,14 +1,18 @@
 from itt.server import Server
 from itt.client import Client
+
 from itt.client.ittclient import IttClient
 from itt.client.httpclient import HttpClient
 from itt.client.tftpclient import TftpClient
 from itt.client.ftpclient import FtpClient
+
 from itt.server.tftpserver import TftpServer
 from itt.server.ftpserver import FtpServer
 from itt.server.httpserver import HttpServer
 from itt.server.httprequesthandler import HttpRequestHandler
 from itt.server.config import ServerConfig
+
+from itt.test import Test
 from itt.test.config import TestConfig
 from itt.test.content import TestContent
 from itt.test.connection import TestConnection
