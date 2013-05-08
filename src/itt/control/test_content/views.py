90d2aab631856a378152e29948870c8ff7ff9332
Lou Markovski <lou.markovski@softelsystems.com.au>
2013-05-08 19:12:57 +1000

    In general: * feature/us39 task/ta46 -- Create the test_content view
    control/static/itt_nav.css
    control/templates/base.html
    * added "testcontent" reference to the navigation bar
    
    control/urls.py
    * updated URLconf to support "testcontent" URL
    
    control/test_content/views.py
    * created a simple index view
    
    control/static/test_content.css
    * new test content app-specific CSS
    
    control/templates/test_content/
    * index.html template to support the index view
    
    control/test_content/forms.py
    * form support for test content via the TestContentForm

--- src/itt/control/test_content/views.py
+++ src/itt/control/test_content/views.py
@@ -1,1 +1,27 @@
-# Create your views here.
+from django.template import (RequestContext,
+                             loader)
+from django.http import HttpResponse
+
+from test_content.models import TestContent
+from test_content.forms import TestContentForm
+
+
+def index(request):
+    """
+    """
+    test_content_list = TestContent.objects.all()
+    response = _index_get(request, test_content_list)
+
+    return response
+
+def _index_get(request, test_content_list):
+    """
+    """
+    t = loader.get_template('test_content/index.html')
+
+    form = TestContentForm()
+    c = RequestContext(request,
+                       {'form': form,
+                        'test_content_list': test_content_list})
+
+    return HttpResponse(t.render(c))
