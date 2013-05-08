5d737a8c8aa95c1274b66b751241d35b3e07dbec
Lou Markovski <lou.markovski@softelsystems.com.au>
2013-05-08 20:43:41 +1000

    In general: * feature/us39 task/ta47 -- Create the test_content insert form
    control/test_config/views.py
    * factored out call to TestConfig.object.all() in the index() view to the
      _index_get() function as it was not required for a POST request
    
    test_content/tests/__init__.py
    * add TestTestContentViews into the test_content tests namespace
    
    test_content/views.py
    * added the _index_post() function to support POST requests

--- src/itt/control/test_content/views.py
+++ src/itt/control/test_content/views.py
@@ -1,6 +1,7 @@
 from django.template import (RequestContext,
                              loader)
-from django.http import HttpResponse
+from django.http import (HttpResponse,
+                         HttpResponseRedirect)
 
 from test_content.models import TestContent
 from test_content.forms import TestContentForm
@@ -9,14 +10,17 @@ from test_content.forms import TestContentForm
 def index(request):
     """
     """
-    test_content_list = TestContent.objects.all()
-    response = _index_get(request, test_content_list)
+    if request.method == 'POST':
+        response = _index_post(request)
+    else:
+        response = _index_get(request)
 
     return response
 
-def _index_get(request, test_content_list):
+def _index_get(request):
     """
     """
+    test_content_list = TestContent.objects.all()
     t = loader.get_template('test_content/index.html')
 
     form = TestContentForm()
@@ -25,3 +29,21 @@ def _index_get(request, test_content_list):
                         'test_content_list': test_content_list})
 
     return HttpResponse(t.render(c))
+
+def _index_post(request):
+    """
+    """
+    # Note: the 'cancel' submit will fall through to the return.
+    try:
+        if request.POST['submit']:
+            form = None
+
+            if request.POST['submit'] == 'Add Content':
+                form = TestContentForm(request.POST)
+
+            if form:
+                form.save()
+    except KeyError:
+        pass
+
+    return HttpResponseRedirect('/testcontent/')
