723c36fb7d0e196ac9df5f869434e6f946feeca8
Philip Jay <philip.jay@softelsystems.com.au>
2013-05-09 12:30:53 +1000

    Add functionality to make use of itt.test objects

--- src/itt/server/httprequesthandler.py
+++ src/itt/server/httprequesthandler.py
@@ -5,6 +5,8 @@
 import hashlib
 import BaseHTTPServer
 
+import os
+import urlparse
 import itt
 from itt.utils.log import log, class_logging
 
@@ -13,6 +15,11 @@ class HttpRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     """
     
     contentSent = ''
+
+    RANDOM_PATH = '/testing/dev/random'
+    NULL_PATH = '/testing/dev/null'
+
+    DEFAULT_BYTES = 1024
     
     def storeAndWrite(self, str, wfile):
         """Store the string in self.contentSent, and write it to wfile)"""
@@ -51,11 +58,40 @@ class HttpRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             self.path,
         ))
 
+        url = urlparse.urlparse(self.path)
+        qs = urlparse.parse_qs(url.query)
+
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
 
-        ##  XXX: Todo, new functionality here
+        ##  Check for "special" paths, otherwise try and read real files
+        if url.path == self.RANDOM_PATH:
+
+            ##  Setup test content
+            bytes = self.DEFAULT_BYTES
+            if 'bytes' not in qs:
+                log.warning("Query string does not specify 'bytes', but random data requested")
+                log.warning("Using %s bytes by default" % self.DEFAULT_BYTES)
+            else:
+                bytes = qs['bytes']
+
+            content = itt.TestContent(None, bytes=bytes)
+
+            ##  Setup test configuration
+            config = itt.TestConfig()
+            if 'minimum_gap' in qs:
+                config.minimum_gap = qs['minimum_gap']
+            if 'chunk_size' in qs:
+                config.chunk_size = qs['chunk_size']
+
+        else if url.path == self.NULL_PATH:
+            pass
+        else:
+            pass
+
+        ##  connection = None, as the client has created the connection
+        test = itt.Test(config, content, None)
 
         log.info("HTTP GET request finished for %s on port %s for %s\n   Resulting SHA1 sum of content: %s" % (
            self.client_address[0],
