aa1e5837844257dd55f085105dffa41e1b1104ca
Philip Jay <philip.jay@softelsystems.com.au>
2013-05-09 12:29:08 +1000

    Remove old proof of concept code

--- src/itt/server/httprequesthandler.py
+++ src/itt/server/httprequesthandler.py
@@ -51,36 +51,12 @@ class HttpRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             self.path,
         ))
 
-        path_array = self.path.split('/')
-
         self.send_response(200)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
 
-        self.storeAndWrite(("Selected path %s\n" % path_array[1]), self.wfile)
+        ##  XXX: Todo, new functionality here
 
-        if path_array[1] == 'fast':
-            self.storeAndWrite(("Fastest possible transfer *\n"), self.wfile)
-            self.storeAndWrite(("* as limited by the TCP stack & network\n"), self.wfile)
-            self.wfile.flush()
-            for i in range(1000):
-                self.storeAndWrite(("Line #%s / 1000\n" % i), self.wfile)
-            self.wfile.flush()
-
-        elif path_array[1] == 'slow':
-            self.storeAndWrite(("Slow transfer *\n"), self.wfile)
-            self.storeAndWrite(("* a few bytes every 1 second\n"), self.wfile)
-            self.wfile.flush()
-            for i in range(10):
-                self.storeAndWrite(("Line #%s / 1000\n" % i), self.wfile)
-                time.sleep(1)
-                self.wfile.flush()
-                
-        else:
-            self.storeAndWrite(("You chose %s, which is not valid\n" % path_array[1]), self.wfile)
-            self.storeAndWrite(("Try fast or slow\n"), self.wfile)
-            self.wfile.flush()
-            
         log.info("HTTP GET request finished for %s on port %s for %s\n   Resulting SHA1 sum of content: %s" % (
            self.client_address[0],
            self.client_address[1],
